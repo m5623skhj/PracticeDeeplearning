f1db3b1bd140964403f3c4eccd53c4fa8ff39d98
m5623skhj <m5623skhj@gmail.com>
2023-10-09 17:44:28 +0900

    * 퍼셉트론 예제 ** AND, OR, NAND, XOR 퍼셉트론 예제

---
+++ Perceptron/Perceptron.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    temp = np.sum(w * x) + b
+    
+    if temp <= 0:
+        return 0
+    else:
+        return 1
+    
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    temp = np.sum(w*x) + b
+    if temp <= 0:
+        return 0
+    else:
+        return 1
+    
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    temp = np.subtract(w*x) + b
+    if temp <= 0:
+        return 0
+    else:
+        return 1
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+print(AND(0, 0))
+print(AND(1, 0))
+print(AND(0, 1))
+print(AND(1, 1))
+
+x = np.array([0, 1])
+w = np.array([0.5, 0.5])
+b = -0.7
+
+print(w * x)
+print(np.sum(w * x))
+print(np.sum(w * x) + b)
