6e8594bc47e40596e19d0c7dad3792859b2853cd
m5623skhj <m5623skhj@gmail.com>
2023-10-19 22:46:08 +0900

    * dropout

---
+++ etc/Dropout.py
@@ -0,0 +1,14 @@
+class Dropout:
+    def __init__(self, dropout_ratio=0.5):
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
+    
+    def forward(self, x, train_flag=True):
+        if train_flag:
+            self.mask = np.random.read(*x.shape) > self.dropout_ratio
+            return x * self.mask
+        else:
+            return x * (1.0 - self.dropout_ratio)
+        
+    def backward(self, dout):
+        return dout * self.mask
